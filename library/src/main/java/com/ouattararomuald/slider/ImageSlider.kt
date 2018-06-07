e5bbfb742cb8904f71ecf56a4ca4b34366a40244
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-07 23:47:33 +0200

    Creates ImageSlider

---
+++ library/src/main/java/com/ouattararomuald/slider/ImageSlider.kt
@@ -0,0 +1,56 @@
+package com.ouattararomuald.slider
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+
+class ImageSlider : LinearLayout {
+
+  private lateinit var viewPager: ViewPager
+  private lateinit var indicator: TabLayout
+
+  private val emptyCallback = object : Callback {
+    override fun loadImageFor(imageView: ImageView, position: Int) {
+    }
+  }
+
+  private var indicatorSetupWithPager = false
+
+  var adapter: SliderAdapter? = null
+    set(value) {
+      if (value != null) {
+        field = value
+        viewPager.adapter = field
+
+        setupIndicatorWithViewPagerIfNecessary()
+      }
+    }
+
+  constructor(context: Context) : this(context, null)
+
+  constructor(context: Context, attrs: AttributeSet?) : super(context, attrs, 0)
+
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+      : super(context, attrs, defStyleAttr) {
+    LayoutInflater.from(context).inflate(R.layout.slider, this, true)
+
+    viewPager = findViewById(R.id.view_pager)
+    indicator = findViewById(R.id.indicator)
+
+    val attributes: TypedArray = context.theme.obtainStyledAttributes(
+        attrs, R.styleable.ImageSlider, defStyleAttr, 0
+    )
+  }
+
+  private fun setupIndicatorWithViewPagerIfNecessary() {
+    if (!indicatorSetupWithPager) {
+      indicator.setupWithViewPager(viewPager, true)
+      indicatorSetupWithPager = true
+    }
+  }
+}
