c082b5eca0347180b0ca140e6273e71b04da8f3e
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-08 14:49:12 +0200

    Add background attributes

--- library/src/main/java/com/ouattararomuald/slider/ImageSlider.kt
+++ library/src/main/java/com/ouattararomuald/slider/ImageSlider.kt
@@ -4,14 +4,24 @@ import android.content.Context
 import android.content.res.TypedArray
 import android.support.constraint.ConstraintLayout
 import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewCompat
 import android.support.v4.view.ViewPager
+import android.support.v7.content.res.AppCompatResources
 import android.util.AttributeSet
 import android.view.LayoutInflater
 
+/**
+ *
+ * @attr ref com.ouattararomuald.slider.R.styleable#ImageSlider_indicatorBackground
+ * @attr ref com.ouattararomuald.slider.R.styleable#ImageSlider_sliderBackground
+ */
 class ImageSlider : ConstraintLayout {
 
   private val viewPager: ViewPager
   private val indicator: TabLayout
+
+  private var sliderBackgroundResId: Int = 0
+  private var indicatorBackgroundResId: Int = 0
 
   private var indicatorSetupWithPager = false
 
@@ -39,6 +49,28 @@ class ImageSlider : ConstraintLayout {
     val attributes: TypedArray = context.theme.obtainStyledAttributes(
         attrs, R.styleable.ImageSlider, defStyleAttr, 0
     )
+
+    attributes.apply {
+      indicatorBackgroundResId = getResourceId(
+          R.styleable.ImageSlider_indicatorBackground, 0
+      )
+      sliderBackgroundResId = getResourceId(R.styleable.ImageSlider_sliderBackground, 0)
+    }
+
+    if (indicatorBackgroundResId != 0) {
+      ViewCompat.setBackground(
+          indicator, AppCompatResources.getDrawable(context, indicatorBackgroundResId)
+      )
+    }
+
+    if (sliderBackgroundResId != 0) {
+      ViewCompat.setBackground(
+          this, AppCompatResources.getDrawable(context, sliderBackgroundResId)
+      )
+      ViewCompat.setBackground(
+          viewPager, AppCompatResources.getDrawable(context, sliderBackgroundResId)
+      )
+    }
   }
 
   private fun setupIndicatorWithViewPagerIfNecessary() {
