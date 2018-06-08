8db2d140d4252d12cc48c0c012e9a8eef7b485b4
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-08 19:30:07 +0200

    Add animation

--- app/src/main/java/com/slider/demo/MainActivity.kt
+++ app/src/main/java/com/slider/demo/MainActivity.kt
@@ -2,25 +2,32 @@ package com.slider.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ImageView
-import com.ouattararomuald.slider.ImageLoaderCallback
-import com.ouattararomuald.slider.ImageSlider
-import com.ouattararomuald.slider.SliderAdapter
-import com.squareup.picasso.Picasso
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
+import com.xwray.groupie.ViewHolder
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var imageSlider: ImageSlider
+  private lateinit var recyclerView: RecyclerView
+  private val groupAdapter = GroupAdapter<ViewHolder>()
+  private val slidersSection = Section()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    imageSlider = findViewById(R.id.image_slider)
-    imageSlider.adapter = SliderAdapter(this, 10, object: ImageLoaderCallback {
-      override fun loadImageFor(imageView: ImageView, position: Int) {
-        Picasso.get().load(Data.URLS[position]).into(imageView)
-      }
-    })
+    groupAdapter.add(slidersSection)
+
+    recyclerView = findViewById(R.id.recycler_view)
+    recyclerView.apply {
+      layoutManager = GridLayoutManager(this@MainActivity, 2)
+      adapter = groupAdapter
+    }
+
+    Data.URLS.forEach { urls ->
+      slidersSection.add(SliderItem(urls))
+    }
   }
 }
