0df7ee2ade15aa30d1b3248a3648efe308f00ffd
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-13 23:39:28 +0200

    Refactorize

--- slider/src/main/java/com/ouattararomuald/slider/AnimationGenerator.kt
+++ slider/src/main/java/com/ouattararomuald/slider/AnimationGenerator.kt
@@ -6,17 +6,20 @@ import android.view.animation.TranslateAnimation
 
 internal class AnimationGenerator private constructor() {
   companion object {
+    private const val DEFAULT_ANIMATION_DURATION_IN_MILLIS = 500L
+
     fun generateEntranceAnimation(view: View, animationType: AnimationType): Animation {
       when (animationType) {
         AnimationType.TRANSLATION -> {
+          val toYDelta = 0f
           val translateAnimation = TranslateAnimation(
               view.x,
               view.x,
               view.height.toFloat(),
-              0f
+              toYDelta
           )
           translateAnimation.apply {
-            duration = 500
+            duration = DEFAULT_ANIMATION_DURATION_IN_MILLIS
             fillAfter = true
           }
 
@@ -28,14 +31,15 @@ internal class AnimationGenerator private constructor() {
     fun generateExitAnimation(view: View, animationType: AnimationType): Animation {
       when (animationType) {
         AnimationType.TRANSLATION -> {
+          val fromYDelta = 0f
           val translateAnimation = TranslateAnimation(
               view.x,
               view.x,
-              0f,
+              fromYDelta,
               view.height.toFloat()
           )
           translateAnimation.apply {
-            duration = 500
+            duration = DEFAULT_ANIMATION_DURATION_IN_MILLIS
             fillAfter = true
           }
 
