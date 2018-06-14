53613c3515f6da53ae0d33081da64695c0dd1f9d
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-15 00:49:59 +0200

    Removes useless load methods

--- slider-library/src/main/java/com/ouattararomuald/slider/ImageLoader.kt
+++ slider-library/src/main/java/com/ouattararomuald/slider/ImageLoader.kt
@@ -1,26 +1,12 @@
 package com.ouattararomuald.slider
 
-import android.net.Uri
-import android.support.annotation.DrawableRes
 import android.widget.ImageView
-import java.io.File
 
 /** Loads image into [ImageView]. */
 interface ImageLoader {
 
   /** Loads an image into the given [imageView] using the specified path. */
   fun load(path: String, imageView: ImageView)
-
-  /** Loads an image into the given [imageView] using the specified image file. */
-  fun load(file: File, imageView: ImageView)
-
-  /** Loads an image into the given [imageView] using the specified URI. */
-  fun load(uri: Uri, imageView: ImageView)
-
-  /**
-   * Loads an image into the given [imageView] using the specified resource drawable resource ID.
-   */
-  fun load(@DrawableRes resourceId: Int, imageView: ImageView)
 
   /** Creates an [ImageLoader] instance. */
   abstract class Factory<T : ImageLoader> {
