49c9e36545d126b2ed0cac9b3a9307f1c14ba745
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-15 00:30:37 +0200

    Add tests to verify initialization of slider adapter

--- slider-library/src/androidTest/java/com/ouattararomuald/slider/SliderAdapterTest.kt
+++ slider-library/src/androidTest/java/com/ouattararomuald/slider/SliderAdapterTest.kt
@@ -1,22 +1,60 @@
 package com.ouattararomuald.slider
 
+import android.content.Context
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import android.widget.ImageView
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SliderAdapterTest {
+  companion object {
+    const val QUANTITY = 5
+  }
+
+  private lateinit var appContext: Context
+  private lateinit var sliderAdapter: SliderAdapter
+
+  @Before
+  fun setUp() {
+    appContext = InstrumentationRegistry.getTargetContext()
+  }
 
   @Test(expected = IllegalArgumentException::class)
-  fun sliderAdapterWithZeroOrLessSlidesShouldFails() {
-    val appContext = InstrumentationRegistry.getTargetContext()
+  fun sliderAdapterWithZeroImageShouldFails() {
+    val sliderAdapter = SliderAdapter(
+        appContext,
+        PicassoImageLoaderFactory(),
+        imageUrls = arrayListOf()
+    )
+  }
 
-    val sliderAdapter = SliderAdapter(appContext, -1, object : ImageLoaderCallback {
-      /** Loads image for the [imageView] at the given [position]. */
-      override fun loadImageFor(imageView: ImageView, position: Int) {
-      }
-    })
+  @Test(expected = IllegalArgumentException::class)
+  fun sliderAdapterWithDifferentImageSizeAndDescriptionsSizeShouldFails() {
+    val sliderAdapter = createSliderAdapter(
+        numberOfImages = QUANTITY,
+        numberOfDescriptions = QUANTITY * 2
+    )
+  }
+
+  @Test
+  fun sliderAdapterWithSameImagesSizeAndDescriptionsSizeShouldSuccess() {
+    val sliderAdapter = createSliderAdapter(
+        numberOfImages = QUANTITY,
+        numberOfDescriptions = QUANTITY
+    )
+
+    assertThat(sliderAdapter).isNotNull()
+  }
+
+  private fun createSliderAdapter(numberOfImages: Int, numberOfDescriptions: Int): SliderAdapter {
+    return SliderAdapter(
+        appContext,
+        PicassoImageLoaderFactory(),
+        imageUrls = DataGenerator.generateTexts(quantity = numberOfImages),
+        descriptions = DataGenerator.generateTexts(quantity = numberOfDescriptions)
+    )
   }
 }
