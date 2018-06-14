11cb4be4d5972e17486e36a354a89f932516f3fa
Ouattara Romuald <ouattararomuald@yahoo.fr>
2018-06-14 23:17:07 +0200

    Use picasso image loader factory

--- sample/src/main/java/com/slider/demo/SliderItem.kt
+++ sample/src/main/java/com/slider/demo/SliderItem.kt
@@ -1,10 +1,8 @@
 package com.slider.demo
 
 import android.support.v4.view.ViewPager
-import android.widget.ImageView
-import com.ouattararomuald.slider.ImageLoaderCallback
+import com.ouattararomuald.slider.PicassoImageLoaderFactory
 import com.ouattararomuald.slider.SliderAdapter
-import com.squareup.picasso.Picasso
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.slider_item.slider
@@ -16,13 +14,9 @@ internal class SliderItem(
 
   override fun bind(viewHolder: ViewHolder, position: Int) {
     viewHolder.slider.adapter = SliderAdapter(viewHolder.slider.context,
-        imageUrls.size,
-        object : ImageLoaderCallback {
-          override fun loadImageFor(imageView: ImageView, position: Int) {
-            Picasso.get().load(imageUrls[position]).fit().into(imageView)
-          }
-        },
-        Data.generateDescriptions(imageUrls.size)
+        PicassoImageLoaderFactory(),
+        imageUrls = imageUrls.toList(),
+        descriptions = Data.generateDescriptions(imageUrls.size)
     )
     viewHolder.slider.pageTransformer = pageTransformer
   }
